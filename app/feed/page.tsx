288365bcbe452d4a00d2be0b62899f9d1d8fdd8a
Kevin <kevin.larner@punchcard.io>
2024-09-18 11:51:50 -0600

    dummy data

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Feed() {
     const cardHeader = ["Dog Name 1", "Dog Name 2", "Dog Name 3", "Dog Name 4", "Dog Name 245", "Dog Name 4564563"];
-    const cardBody = ["If a dog chews shoes whose shoes does he choose?", "Dog Card body 2", "Dog Card body 3"];
+    const cardBody = ["If a dog chews shoes whose shoes does he choose?", "Dog Card body 2", "Dog Card body 3", "Dog Card body 4", "Dog Card body 5", "Dog Card body 6"];
 
     return (
         <div className='flex flex-row gap-4 max-w-7xl flex-wrap justify-center'>
